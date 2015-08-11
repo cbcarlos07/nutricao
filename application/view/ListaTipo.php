f8fa674c6487605689c2edf357772787f525b989
master <ham139>
2015-08-11 15:17:51 -0400

    view.

--- application/view/ListaTipo.php
+++ application/view/ListaTipo.php
@@ -44,6 +44,7 @@
                                 </tr>
                                 </thead>
                                 <tbody>
+                                
                                 <?php
 
                                         while ($list->hasNextTipo()) {
