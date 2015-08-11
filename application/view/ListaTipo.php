c5e1ab3c23676f9ba1a6d96c24b629b36de59040
master <ham139>
2015-08-11 15:17:20 -0400

    view.

--- application/view/ListaTipo.php
+++ application/view/ListaTipo.php
@@ -9,18 +9,7 @@
 
    </head>
 <body>
-        <table class="table table-bordered">
-            <caption>painel de controle</caption>
-            <thead>
-            <tr bgcolor="#abaf9b">
-                <th>C&Oacute;DIGO</th>
-                <th >DESCRI&Ccedil;&Atilde;O</th>
-                <th ALIGN="CENTER">ALTERAR</th>
-                <th ALIGN="CENTER">EXCLUIR</th>
 
-            </tr>
-            </thead>
-            <tbody>
                             <?php
                             //$_SERVER['DOCUMENT_ROOT'].'rh/application
 
@@ -36,27 +25,53 @@
                             $tipo = new Tipo_Ref();
 
                             $i = 0;
+                            $registros = 0;
                             while($list->hasNextTipo()){
-                                $i++;
-                                if($i % 2 == 0){
-                                    $par = "#cdd4aa";
-                                }
-                                else{
-                                    $par = "#fff";
-                                }
                                 $tipo = $list->getNextTipo();
-                                echo "<tr bgcolor=$par>";
-                                echo "<td>".$tipo->getId()."</td>";
-                                echo "<td>".$tipo->getDescricao()."</td>";
-                                echo "<td align=center><a href=AcaoPesquisa.php?acao='A'&code=".$pesquisa->getCd_Pesquisa()." title='Alterar Pesquisa' rel='facebox' > <img src=../../public/img/alterar.png width=20 height=20></a></td>";
-                                echo "<td align=center><a href=# onClick=redirecionar(".$pesquisa->getCd_Pesquisa().") title='Deletar Pesquisa'> <img src=../../public/img/delete.png width=20 height=20></a></td>";
-                                echo "</tr>";
-
+                                $registros++;
                             }
+                            if($registros>0) {
+                            ?>
+                            <table class="table table-bordered">
+                                <caption>painel de controle</caption>
+                                <thead>
+                                <tr bgcolor="#abaf9b">
+                                    <th>C&Oacute;DIGO</th>
+                                    <th >DESCRI&Ccedil;&Atilde;O</th>
+                                    <th ALIGN="CENTER">ALTERAR</th>
+                                    <th ALIGN="CENTER">EXCLUIR</th>
+
+                                </tr>
+                                </thead>
+                                <tbody>
+                                <?php
+
+                                        while ($list->hasNextTipo()) {
+                                            $i++;
+                                            if ($i % 2 == 0) {
+                                                $par = "#cdd4aa";
+                                            } else {
+                                                $par = "#fff";
+                                            }
+                                            $tipo = $list->getNextTipo();
+                                            echo "<tr bgcolor=$par>";
+                                            echo "<td>" . $tipo->getId() . "</td>";
+                                            echo "<td>" . $tipo->getDescricao() . "</td>";
+                                            echo "<td align=center><a href=AcaoPesquisa.php?acao='A'&code=" . $pesquisa->getCd_Pesquisa() . " title='Alterar Pesquisa' rel='facebox' > <img src=../../public/img/alterar.png width=20 height=20></a></td>";
+                                            echo "<td align=center><a href=# onClick=redirecionar(" . $pesquisa->getCd_Pesquisa() . ") title='Deletar Pesquisa'> <img src=../../public/img/delete.png width=20 height=20></a></td>";
+                                            echo "</tr>";
+
+                                        }
+                                ?>
+                                </tbody>
+
+                            </table>
+                            <?php
+                                }else{
+                                    echo "N&atilde;o existem dados a serem mostrados";
+                                }
 
                             ?>
-            </tbody>
 
-        </table>
 </body>
 </html>
