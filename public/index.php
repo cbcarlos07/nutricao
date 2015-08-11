2361efef1b1b58f967ccb6d24d63c614ba84e3b3
leandersonti <ti.sistemas.leanderson@gmail.com>
2015-08-11 15:15:19 -0400

    dfghj

--- public/index.php
+++ public/index.php
@@ -26,14 +26,40 @@
 
         <div class="span10 corpo">
             <div class="tabbable">
-                <ul class="nav nav-tabs">
+                <ul class="nav nav-tabs menu-tab">
 
-                    <li class="tab-pane active"><a href="" data-toggle>cardapio</li>
-                    <li class="tab-pane"><a href="" data-toggle>agendamento</li>
-                    <li class="tab-pane"><a href="" data-toggle>etapa1</li>
-                    <li class="tab-pane"><a href="" data-toggle>etapa2</li>
-                    <li class="tab-pane"><a href="" data-toggle>etapa3</li>
+                    <li class="active"><a href="" data-toggle>cardapio</li>
+                    <li><a href="" data-toggle>agendamento</li>
+                    <li><a href="" data-toggle>etapa1</li>
+                    <li><a href="" data-toggle>etapa2</li>
+                    <li><a href="" data-toggle>etapa3</li>
                 </ul>
+
+                <div class="tab-content">
+                    <div class="tab-pane active" id="tab1">
+                        <p>teste1</p>
+                    </div>
+
+
+
+
+                </div>
+
+            </div>
+
+            <div class="tabbable">
+                <ul class="nav nav-tabs">
+                    <li class="active"><a href="#tab1" data-toggle="tab">Secao 1</a></li>
+                    <li><a href="#tab2" data-toggle="tab">Secao 2</a></li>
+                </ul>
+                <div class="tab-content">
+                    <div class="tab-pane active" id="tab1">
+                        <p>Estou na secao 1</p>
+                    </div>
+                    <div class="tab-pane" id="tab2">
+                        <p>Ola, estou na secao 2</p>
+                    </div>
+                </div>
             </div>
 
 
