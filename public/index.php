087c1fde9662b98920072cfe28dacca144a2a131
leandersonti <ti.sistemas.leanderson@gmail.com>
2015-08-11 13:45:24 -0400

    LEANDERSON 23

--- public/index.php
+++ public/index.php
@@ -1,7 +1,49 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: leanderson.silva
- * Date: 10/08/2015
- * Time: 15:30
- */
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <link href="css/bootstrap.css" rel="stylesheet" type="text/css">
+    <link href="css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
+    <link href="css/style.css" type="text/css" rel="stylesheet">
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+
+
+    <title>SISCONALT (Sistema de Controle de alimentos)</title>
+</head>
+<body>
+<div class="container">
+
+    <div class="row">
+
+        <div class="span10 head">
+            <h1 ALIGN="CENTER">CADASTRO DE ALIMENTOS</h1>
+
+        </div>
+
+    </div>
+
+    <div class="row">
+
+        <div class="span10 corpo">
+            <div class="tabbable">
+                <ul class="nav nav-tabs">
+
+                    <li class="tab-pane active"><a href="" data-toggle>cardapio</li>
+                    <li class="tab-pane"><a href="" data-toggle>agendamento</li>
+                    <li class="tab-pane"><a href="" data-toggle>etapa1</li>
+                    <li class="tab-pane"><a href="" data-toggle>etapa2</li>
+                    <li class="tab-pane"><a href="" data-toggle>etapa3</li>
+                </ul>
+            </div>
+
+
+        </div>
+
+    </div>
+
+
+
+</div>
+
+</body>
+</html>
